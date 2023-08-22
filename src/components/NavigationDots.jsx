7f0a637c7f02391c81e2ed2db0333a37c4805710
Byakkuya (ahmed zbaa) <88725196+Byakkuya@users.noreply.github.com>
2023-08-22 10:46:35 +0000

    fixing the navigation dots

--- src/components/NavigationDots.jsx
+++ src/components/NavigationDots.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 
 const NavigationDots = ({ active }) => (
   <div className="app__navigation">
-    {['home', 'about','skills','work', 'contact'].map((item, index) => (
+    {['home', 'about','skills','work', 'testimonial','contact'].map((item, index) => (
       <a
         href={`#${item}`}
         key={item + index}
